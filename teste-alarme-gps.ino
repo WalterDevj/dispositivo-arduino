aac6377c869a429418520b668cd30c8126be4b72
Walter <115043746+WalterDevj@users.noreply.github.com>
2024-11-28 15:09:56 -0300

    Update teste-alarme-gps.ino

--- teste-alarme-gps.ino
+++ teste-alarme-gps.ino
@@ -2,15 +2,15 @@
 #include <SoftwareSerial.h>
 
 const int RX_PIN = 6, TX_PIN = 5;
-const uint32_t GPS_BAUD = 9600; //Default baud of NEO-6M is 9600
+const uint32_t GPS_BAUD = 9600; //Entrada padrão do NEO-6M é 9600
 
 
 TinyGPSPlus gps; // The TinyGPS++ object
 SoftwareSerial gpsSerial(RX_PIN, TX_PIN);
 
-int buttonpin= 9;   // declare the variable for the store button pin
-int buzzerpin= 8;  // declare variable for the store buzzer pin 
-int pinstatus;     // declare variable to store the pin status
+int buttonpin= 9;   // Declara a variável do botão no pino digital 9
+int buzzerpin= 8;  // declara a variável para armazenar o alarme no pino digital 8
+int pinstatus;     // declare a variável para o estado do dos pinos
 void setup() {
   pinMode(buttonpin, INPUT);
   pinMode(buzzerpin,OUTPUT);
@@ -22,12 +22,12 @@ void setup() {
 }
 void loop() 
 {
- pinstatus=digitalRead(buttonpin);    // read the status of digital pin and stores the value of pin status
- if (pinstatus==HIGH)                 // condition statement which checks the pin status, pinstatus high means       
- {                                  //   when we press the button switch values in the pin status becomes HIGH
-  digitalWrite(buzzerpin,HIGH);
-  if (gpsSerial.available() > 0) {
-    if (gps.encode(gpsSerial.read())) {
+ pinstatus=digitalRead(buttonpin);    // Lê o estado da entrada digital e armazena o valor da variável dpinstatus
+ if (pinstatus==HIGH)                 // Declara condição que verifica o status da entrada, pinstatus HIGH define   
+ {                                  //   que quando o botão for apertado o valor do pinstatus vai se tornar HIGH/"ativo"
+  digitalWrite(buzzerpin,HIGH); // Declara que quando estado da entrada for HIGH, então o buzzer/alarme vai ser declarado como HIGH e ele vai ser acionado
+  if (gpsSerial.available() > 0) { // Ativando o Módulo de GPS que verifica com o uso da biblioteca TinyGPS++ latitude, altitude, velocidade e declara data e hora
+    if (gps.encode(gpsSerial.read())) { //Quando o valor for diferente de 0 realiza a leitura das informações dadas pelo GPS
       if (gps.location.isValid()) {
         Serial.print(F("- latitude: "));
         Serial.println(gps.location.lat());
@@ -49,7 +49,7 @@ void loop()
         Serial.print(gps.speed.kmph());
         Serial.println(F(" km/h"));
       } else {
-        Serial.println(F("INVALID"));
+        Serial.println(F("INVALID")); //Caso não esteja recebendo a informação aparecerá uma mensagem dando como "inválido" no monitor serial
       }
 
       Serial.print(F("- GPS date&time: "));
@@ -69,17 +69,17 @@ void loop()
         Serial.println(F("INVALID"));
       }
 
-      Serial.println();
+      Serial.println(); //
     }
   }
 
   if (millis() > 5000 && gps.charsProcessed() < 10)
     Serial.println(F("No GPS data received: check wiring"));
 
-       // turn on the buzzer
+       // Liga o alarme (buzzer)
  }
  else
  {
-  digitalWrite(buzzerpin,LOW);     // turn off the buzzer
+  digitalWrite(buzzerpin,LOW);     // Desliga o alarme (buzzer)
  }
 }
