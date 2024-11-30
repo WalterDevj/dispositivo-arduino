5aa0391ec23a844ac9ba38e2b5addd0e70afe6da
Walter <115043746+WalterDevj@users.noreply.github.com>
2024-11-30 14:50:55 -0300

    Update teste-botoes.ino

--- teste-botoes.ino
+++ teste-botoes.ino
@@ -1,9 +1,10 @@
-#define buzzer 8
+#define buzzer 8 
 #define botaoBuzzer 9
 #define botaoMensagem 10
+//Definindo Buzzer na porta digital 8, o botão do buzzer na 9 e o botão de mensagem na porta 10
 
 #include <SoftwareSerial.h>
-int pinstatus;     // declare variable to store the pin status
+int pinstatus;     // declarando variável pinstatus
 
 void setup() {
   pinMode(buzzer,OUTPUT);
@@ -11,6 +12,10 @@ void setup() {
   pinMode(botaoBuzzer, INPUT);
   pinMode(botaoMensagem, INPUT_PULLUP);
   Serial.begin(9600);
+
+  //Define a entrada do buzzer como OUTPUT, enquanto o botão do buzzer é definido como input
+  //e o botão da mensagem é definido como input (mas esse é acionado enquanto for pressionado).
+  //Define a "frequência" de entrada como 9600 do monitor serial.
 }
 void loop() 
 {
@@ -18,14 +23,18 @@ void loop()
     {
       Serial.println ("mensgagem");
     }
+  //Função se, aonde enquanto o botão de mensagem for pressionado, a mensagem será enviada para o monitor seria.
+  //! faz com que a mensagem seja enviada apenas quando o 
 
 
-  if (digitalRead(botaoBuzzer))    // read the status of digital pin and stores the value of pin status
-    {                                  //   when we press the button switch values in the pin status becomes HIGH
+  if (digitalRead(botaoBuzzer))    
+    {                                 
       digitalWrite(buzzer,HIGH);
     }
+    //Função se aonde é feita a leitura do botão do buzzer se está sendo apertado ou não
     else
     {
-      digitalWrite(buzzer,LOW);     // turn off the buzzer
+      digitalWrite(buzzer,LOW);     
     }
+  //Caso o botão não esteja sendo apertado o buzzer ficará desligado
 }
